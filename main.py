67749c34c7b474ed65520eebb09b53dc3608d56d
Janu Lingeswaran <Janusan@Lingeswaran.de>
2021-11-06 11:08:58 +0000

    init 2

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
 import os
-import base64
-import requests
+import tweepy
+import pandas as pd
 
 client_key = os.environ['client_key']
 client_secret = os.environ['client_secret']
+bearer_token = os.environ['bearer_token']
+access_token = os.environ['access_token']
+access_token_secret = os.environ['access_token_secret']
 
-key_secret = '{}:{}'.format(client_key, client_secret).encode('ascii')
-b64_encoded_key = base64.b64encode(key_secret)
-b64_encoded_key = b64_encoded_key.decode('ascii')
-
-base_url = 'https://api.twitter.com/'
-auth_url = '{}oauth2/token'.format(base_url)
-
-auth_headers = {
-    'Authorization': 'Basic {}'.format(b64_encoded_key),
-    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-}
-
-auth_data = {
-    'grant_type': 'client_credentials'
-}
-
-auth_resp = requests.post(auth_url, headers=auth_headers, data=auth_data)
-print(auth_resp.status_code)
+auth = tweepy.OAuthHandler(client_key, client_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+cursor = tweepy.Cursor(api.user_timeline, id='JanuWaran', tweet_mode='extended').items(1)
+for x in cursor:
+  print(dir(x))
