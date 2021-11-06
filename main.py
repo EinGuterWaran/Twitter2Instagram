9c02d8006d29742bf77e30b02343ed2c8bf4f66c
Janu Lingeswaran <Janusan@Lingeswaran.de>
2021-11-06 16:33:19 +0000

    counter for csv files

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ import os
 import tweepy
 import pandas as pd
 import numpy as np
+import GetOldTweets3 as got
 
 client_key = os.environ['client_key']
 client_secret = os.environ['client_secret']
 bearer_token = os.environ['bearer_token']
 access_token = os.environ['access_token']
 access_token_secret = os.environ['access_token_secret']
+
+
+def counter():
+    my_file = open("counter.txt", "r+")
+    counter = int(my_file.read())+1
+    my_file.seek(0)
+    my_file.write(str(counter))
+    my_file.truncate()
+    return counter
 
 
 def is_retweet(x):
@@ -35,7 +45,8 @@ def most_liked_tweets(username, how_many, min_likes):
                            tweet_mode='extended').items(how_many)
     tweets = []
     for x in cursor:
-        if not is_retweet(x) and not is_comment(x) and x.favorite_count >= min_likes:
+        if not is_retweet(x) and not is_comment(
+                x) and x.favorite_count >= min_likes:
             favs = x.favorite_count
             retweets = x.retweet_count
             id = x.id
@@ -50,7 +61,33 @@ def most_liked_tweets(username, how_many, min_likes):
     return tweet_df
 
 
-my_best_tweets = most_liked_tweets("JanuWaran", 6701, 10)
+def most_liked_tweets2(username, how_many, min_likes):
+    # Creation of query object
+    tweetCriteria = got.manager.TweetCriteria().setUsername(
+        username).setMaxTweets(how_many)
+    # Creation of list that contains all tweets
+    thetweets = got.manager.TweetManager.getTweets(tweetCriteria)
+    # Creating list of chosen tweet data
+    tweets = []
+    for x in thetweets:
+        if not is_retweet(x) and not is_comment(
+                x) and x.favorite_count >= min_likes:
+            favs = x.favorite_count
+            retweets = x.retweet_count
+            id = x.id
+            when = x.created_at
+            tweets.append([x.full_text, favs, retweets, id, when])
+    tweet_df = pd.DataFrame(np.array(tweets),
+                            columns=['tweet', 'favs', 'retweet', 'id', 'date'])
+    tweet_df.favs = tweet_df.favs.astype(float)
+    tweet_df.retweet = tweet_df.retweet.astype(float)
+    tweet_df = tweet_df.sort_values('favs',
+                                    ascending=False).reset_index(drop=True)
+    return tweet_df
+
+
+#my_best_tweets = most_liked_tweets("JanuWaran", 6701, 30)
+my_best_tweets = most_liked_tweets("JanuWaran", 25, 1)
 print(my_best_tweets)
 print(my_best_tweets.favs)
-my_best_tweets.to_csv('tweets.csv', index=False)
+my_best_tweets.to_csv('tweets'+str(counter())+'.csv', index=False)
