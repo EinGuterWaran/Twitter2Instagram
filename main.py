5ec510a6e8600de0af1fc803b94dd83e04e0f86a
Janu Lingeswaran <Janusan@Lingeswaran.de>
2021-11-06 12:47:46 +0000

    Modularized + CSV

--- main.py
+++ main.py
@@ -22,23 +22,35 @@ def is_comment(x):
     return False
 
 
-auth = tweepy.OAuthHandler(client_key, client_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-cursor = tweepy.Cursor(api.user_timeline,
-                       id='JanuWaran',
-                       tweet_mode='extended').items(10000)
-tweets = []
-for x in cursor:
-    if not is_retweet(x) and not is_comment(x) and x.favorite_count >= 10:
-        favs = x.favorite_count
-        retweets = x.retweet_count
-        id = x.id
-        when = x.created_at
-        tweets.append([x.full_text, favs, retweets, id, when])
-tweet_df = pd.DataFrame(np.array(tweets), columns=['tweet', 'favs', 'retweet', 'id', 'date'])
-tweet_df.favs = tweet_df.favs.astype(float)
-tweet_df.retweet = tweet_df.retweet.astype(float)
-tweet_df = tweet_df.sort_values('favs', ascending=False).reset_index(drop=True)
-print(tweet_df)
-print(tweet_df.favs)
+def connect_to_twitter():
+    auth = tweepy.OAuthHandler(client_key, client_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    return tweepy.API(auth)
+
+
+def most_liked_tweets(username, how_many, min_likes):
+    api = connect_to_twitter()
+    cursor = tweepy.Cursor(api.user_timeline,
+                           id=username,
+                           tweet_mode='extended').items(how_many)
+    tweets = []
+    for x in cursor:
+        if not is_retweet(x) and not is_comment(x) and x.favorite_count >= min_likes:
+            favs = x.favorite_count
+            retweets = x.retweet_count
+            id = x.id
+            when = x.created_at
+            tweets.append([x.full_text, favs, retweets, id, when])
+    tweet_df = pd.DataFrame(np.array(tweets),
+                            columns=['tweet', 'favs', 'retweet', 'id', 'date'])
+    tweet_df.favs = tweet_df.favs.astype(float)
+    tweet_df.retweet = tweet_df.retweet.astype(float)
+    tweet_df = tweet_df.sort_values('favs',
+                                    ascending=False).reset_index(drop=True)
+    return tweet_df
+
+
+my_best_tweets = most_liked_tweets("JanuWaran", 6701, 10)
+print(my_best_tweets)
+print(my_best_tweets.favs)
+my_best_tweets.to_csv('tweets.csv', index=False)
