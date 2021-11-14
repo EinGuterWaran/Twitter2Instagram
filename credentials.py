46f571a37a2122cfbe312729c15398087c7cf6cb
Janu Lingeswaran <Janusan@Lingeswaran.de>
2021-11-14 19:16:55 +0000

    Modularization + Restructuring

---
+++ credentials.py
@@ -0,0 +1,7 @@
+import os
+
+client_key = os.environ['client_key']
+client_secret = os.environ['client_secret']
+bearer_token = os.environ['bearer_token']
+access_token = os.environ['access_token']
+access_token_secret = os.environ['access_token_secret']
