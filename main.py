8cb6c41d79e64cb47aaf2aff05d69f656c37d01b
EinGuterWaran <Janusan@Lingeswaran.de>
2021-11-23 17:44:06 +0100

    Outlines - Posting on Instagram via CLI

--- main.py
+++ main.py
@@ -20,44 +20,48 @@ def command_line_interface():
     while True:
         mode = input("Enter L if you want to generate a list of the best tweets from an account!\n"
                      "Enter LI if you additionally want to generate Instagram worthy images from these tweets!\n"
-                     "Enter I if you want to generate Instagram worthy images from an existing list!\n")
-        if mode in ["L", "l", "LI", "li", "I", "i"]:
+                     "Enter I if you want to generate Instagram worthy images from an existing list!\n"
+                     "Enter P if you want to post a tweet image on Instagram!\n")
+        if mode in ["L", "l", "LI", "li", "I", "i", "P", "p"]:
             break
-    while True:
-        twitter_handle = input("Enter the Twitter username (handle)! ")
-        if twitter_handle != "":
-            break
-        print("Your input is empty.")
-    twitter_name = input(
-        "Enter the name (leave this blank if you want to fetch the original name)! "
-    )
-    if mode not in ["i", "I"]:
+    if mode in ["p", "P"]:
+        print("Posting...")
+    else:
         while True:
-            last_x_tweets = input(
-                "How many recent tweets should be reviewed (Leave empty to use the maximum possible number)? "
-            )
-            if last_x_tweets == "" or checkIfNumber(last_x_tweets):
-                if last_x_tweets == "":
-                    last_x_tweets = 6701
+            twitter_handle = input("Enter the Twitter username (handle)! ")
+            if twitter_handle != "":
                 break
-        while True:
-            min_favs = input("How many likes should the tweets have minimally? ")
-            if checkIfNumber(min_favs):
-                break
-        tweetlist_file = ft.tweets_to_csv(twitter_handle, int(last_x_tweets),
-                                          int(min_favs))
-        if mode in ["LI", "li"]:
-            ti.tweets_to_images(tweetlist_file, twitter_handle, twitter_name, True, True)
-    if mode in ['I', 'i']:
-        filename = input("Enter the file name (without the csv ending)!\n")
-        filename = 'tweet_lists/'+twitter_handle+'/'+filename+'.csv'
-        ti.tweets_to_images(filename, twitter_handle, twitter_name, True,
-                            True)
+            print("Your input is empty.")
+        twitter_name = input(
+            "Enter the name (leave this blank if you want to fetch the original name)! "
+        )
+        if mode not in ["i", "I"]:
+            while True:
+                last_x_tweets = input(
+                    "How many recent tweets should be reviewed (Leave empty to use the maximum possible number)? "
+                )
+                if last_x_tweets == "" or checkIfNumber(last_x_tweets):
+                    if last_x_tweets == "":
+                        last_x_tweets = 6701
+                    break
+            while True:
+                min_favs = input("How many likes should the tweets have minimally? ")
+                if checkIfNumber(min_favs):
+                    break
+            tweetlist_file = ft.tweets_to_csv(twitter_handle, int(last_x_tweets),
+                                              int(min_favs))
+            if mode in ["LI", "li"]:
+                ti.tweets_to_images(tweetlist_file, twitter_handle, twitter_name, True, True)
+        if mode in ['I', 'i']:
+            filename = input("Enter the file name (without the csv ending)!\n")
+            filename = 'tweet_lists/'+twitter_handle+'/'+filename+'.csv'
+            ti.tweets_to_images(filename, twitter_handle, twitter_name, True,
+                                True)
 
 
 if __name__ == "__main__":
     command_line_interface()
     # tweetlist_file = ft.tweets_to_csv("matze_emmo",6701,1000)
-    # TODO In CMD soll angezeigt werden was gerade passiert
+    # TODO in CLI auf Insta posten
     # TODO automatisierte Pipeline + Instagram Post (Facebook Graph API)
     # TODO Wenn Bilder, sollen diese auch mit gespeichert + gepostet werden - Dateiname mit suffix
